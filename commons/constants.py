b773a13151f675af834c150bc75b3f024ff596a8
Adrien Catel <adrien.catel@nist.gov>
2016-12-13 10:28:17 -0500

    add constants
    Change-Id: I0642e122fd2b1d3faec9d486a93cd2cde364bd3e

--- commons/constants.py
+++ commons/constants.py
@@ -1,5 +1,6 @@
 """ All constants needed for xml operation, can be called outside of this package
 """
 
+XML_NAMESPACE = "http://www.w3.org/XML/1998/namespace"
 SCHEMA_NAMESPACE = "http://www.w3.org/2001/XMLSchema"
 LXML_SCHEMA_NAMESPACE = "{" + SCHEMA_NAMESPACE + "}"
