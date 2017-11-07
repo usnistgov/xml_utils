b31f0ccb0176924c315e355cfe7173de4ba676db
pdessauw <philippe.dessauw@nist.gov>
2017-11-07 11:03:33 -0500

    refactor: organise package
    Change-Id: I3e528dd61619f8732af40f390cf6ad4d63d0cecc

---
+++ README.rst
@@ -0,0 +1,17 @@
+xml_utils
+=========
+
+XML utils for the curator core project.
+
+Quick start
+===========
+
+1. Add "xml_utils" to your INSTALLED_APPS setting
+-------------------------------------------------
+
+.. code:: python
+
+    INSTALLED_APPS = [
+      ...
+      'xml_utils',
+    ]
--- setup.py
+++ setup.py
@@ -1,18 +1,33 @@
-import os
+from os import chdir, pardir
+from os.path import join, exists, dirname, normpath, abspath
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
-    required = f.read().splitlines()
+reqs_default = join(dirname(__file__), "requirements.txt")
+reqs_core = join(dirname(__file__), "requirements.core.txt")
+required = []
+
+if exists(reqs_default):
+    with open(reqs_default) as f:
+        required += f.read().splitlines()
+
+if exists(reqs_core):
+    with open(reqs_core) as f:
+        required += f.read().splitlines()
+
+with open(join(dirname(__file__), "README.rst")) as f:
+    long_desc = f.read()
 
 # Allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="xml_utils",
     version="1.0.0-alpha4",
-    description=("XML Utils package",),
+    description="XML utils for the curator core project",
+    long_description=long_desc,
     author="NIST IT Lab",
     author_email="itl_inquiries@nist.gov",
+    url="https://github.com/usnistgov/xml_utils",
     packages=find_packages(),
     include_package_data=True,
     install_requires=required,
