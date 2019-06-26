e4f037ae3727eb39a24c4820a70b3c2b2c162f0a
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-06-26 11:42:42 -0400

    fix: user defined exceptions to string
    Change-Id: Ia1b71c30f9e8450f9743a2efd8b68e129f7d29af

--- xml_utils/commons/exceptions.py
+++ xml_utils/commons/exceptions.py
@@ -8,15 +8,9 @@ class XMLError(Exception):
     def __init__(self, message):
         self.message = message
 
-    def __str__(self):
-        return repr(self.message)
-
 
 class HTMLError(Exception):
     """ Exception raised by HTML parsing
     """
     def __init__(self, message):
         self.message = message
-
-    def __str__(self):
-        return repr(self.message)
