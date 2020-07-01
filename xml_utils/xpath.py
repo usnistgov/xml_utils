61b698c83619ea6ea01428d02801b3dd331ed78c
Philippe Dessauw <philippe.dessauw@nist.gov>
2020-07-01 14:13:43 -0400

    feat: add function to create tree at a given xpath
    Change-Id: I77e209faa24819c4c06b727a63d20ce6192d2dd2

--- xml_utils/xpath.py
+++ xml_utils/xpath.py
@@ -1,5 +1,7 @@
 """ XPath related functions
 """
+from copy import deepcopy
+
 from lxml import etree
 
 from xml_utils.commons import exceptions
@@ -18,3 +20,51 @@ def validate_xpath(xpath):
         etree.XPath(xpath.strip())
     except etree.XPathSyntaxError as e:
         raise exceptions.XPathError(str(e))
+
+
+def create_tree_from_xpath(xpath, xml_tree, namespaces=None, default_xpath=""):
+    """ Create tree at a given xpath. Recursive function
+
+    Args:
+        xpath:
+        xml_tree:
+        namespaces:
+        default_xpath:
+
+    Returns:
+        The resulting tree
+    """
+    # Make a copy of the input tree to avoid modifying it directly
+    xml_tree = deepcopy(xml_tree)
+
+    if xpath == "":  # If the Xpath is empty, return the current xml_tree
+        return xml_tree
+
+    # Split the xpath and extract the first element
+    xpath_elements = xpath.split("/")
+    xpath_element = ""
+
+    while xpath_element == "":  # Avoid retrieving empty xpath elements
+        xpath_element = xpath_elements.pop(0)
+
+    new_xpath = "%s/%s" % (default_xpath, xpath_element)
+
+    try:
+        assert len(xml_tree.xpath(new_xpath, namespaces=namespaces)) == 1
+    except AssertionError as assertionError:
+        if default_xpath == "":  # If this is the root element, raise the error
+            raise AssertionError(assertionError)
+
+        element = xml_tree.xpath(default_xpath, namespaces=namespaces)
+
+        if xpath_element.startswith("@"):
+            element[0].set(xpath_element[1:], "")
+        else:
+            element[0].append(etree.Element(xpath_element))
+
+    return create_tree_from_xpath(
+        "/".join(xpath_elements),
+        xml_tree,
+        namespaces=namespaces,
+        default_xpath=new_xpath,
+    )
